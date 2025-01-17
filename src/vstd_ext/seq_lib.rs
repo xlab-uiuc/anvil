06da51381814acc983f896fa331ed7dda55d2518
catoverflow <55503205+Catoverflow@users.noreply.github.com>
2025-01-17 16:47:00 -0600

    prove seq_pred_false_on_all_elements_implies_empty_filter

--- src/vstd_ext/seq_lib.rs
+++ src/vstd_ext/seq_lib.rs
@@ -103,14 +103,27 @@ pub proof fn seq_unequal_preserved_by_add_auto<A>(suffix: Seq<A>)
     };
 }
 
-#[verifier(external_body)]
 pub proof fn seq_pred_false_on_all_elements_implies_empty_filter<A>(s: Seq<A>, pred: spec_fn(A) -> bool)
     requires forall |e: A| #![auto] s.contains(e) ==> !pred(e),
-    ensures s.filter(pred).len() == 0;
-//
-// TODO: Prove this -- Trivial.
-//
-// If `pred` is false on every element, filter will return an empty sequence.
-//
-
+    ensures s.filter(pred).len() == 0,
+    decreases s.len()
+    // If `pred` is false on every element, filter will return an empty sequence.
+    {
+        reveal(Seq::filter);
+        if s.len() == 0 {
+            assert (s.filter(pred).len() == 0);
+        } else {
+            let subseq = s.drop_last();
+            // prove precondition for subseq and recursive call
+            assert (forall |e: A| subseq.contains(e) ==> !pred(e)) by {
+                assert (forall |i: int| 0 <= i < subseq.len() ==> s.contains(#[trigger] s[i]) ==> !pred(subseq[i]));
+            }
+            seq_pred_false_on_all_elements_implies_empty_filter(subseq, pred);
+            assert (subseq.filter(pred) == s.filter(pred)) by {
+                assert (!pred(s.last())) by {
+                    assert (s.contains(s.last()) ==> !pred(s.last()));
+                };
+            } // s.filter(pred) == subseq.filter(pred) == ... == Seq::empty()
+        }
+    }
 }
